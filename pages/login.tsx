9d085ac22207f7294dbd3446ad4e0b989e356299
brianslab <bdkuhn602@gmail.com>
2023-01-22 17:02:23 -0800

    start login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,17 @@
+import { FcGoogle } from 'react-icons/fc';
+import { Button } from '@/components';
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className='grid place-content-center text-center'>
+      <div className='text-2xl'>Choose a method to login: </div>
+      <Button
+        className='place-content-center text-lg gap-4 hover:shadow-md transition duration-150 ease-in-out'
+        rounded={true}
+      >
+        <FcGoogle />
+        <a>Google</a>
+      </Button>
+    </div>
+  );
 }
