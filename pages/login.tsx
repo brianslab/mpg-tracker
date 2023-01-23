32a03dd54f3f38897e956d90db46f7ca601408d9
brianslab <bdkuhn602@gmail.com>
2023-01-22 19:08:04 -0800

    styling changes

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components';
 
 export default function Login() {
   return (
-    <div className='grid place-content-center text-center'>
+    <div className='pt-4 space-y-4 grid place-content-center text-center'>
       <div className='text-2xl'>Choose a method to login: </div>
       <Button
         className='place-content-center text-lg gap-4 hover:shadow-md transition duration-150 ease-in-out'
