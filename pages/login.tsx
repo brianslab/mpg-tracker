568b8f3d35b943bfb46cf1ecc5c9b92781c3796a
brianslab <bdkuhn602@gmail.com>
2023-02-05 22:35:08 -0800

    basic login form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,10 @@
-import { Button, Form } from '@/components';
+import { Form } from '@/components';
 
 export default function Login() {
   return (
     <div className='pt-4 space-y-4 grid place-content-center text-center'>
       <div className='text-2xl'>Enter your credentials:</div>
-      <Form />
+      <Form type='login' />
     </div>
   );
 }
